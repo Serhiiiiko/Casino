ca37d9b8e7bac7f3704f8b1effceed6e957777bd
Serhii Maslov <2021elit0049@ms.sumdu.edu.ua>
2025-01-09 16:40:34 +0200

    Lang update 3

--- resources/js/Components/UI/LanguageSelector.vue
+++ resources/js/Components/UI/LanguageSelector.vue
@@ -45,19 +45,42 @@
                         </div>
                     </div>
                 </div>
-<!--                <div class="relative my-4 flex items-center justify-center">-->
-<!--                    <div class="relative">-->
-<!--                        <input type="radio" v-model="language" value="es" name="language_selection" class="peer absolute start-0 top-0 z-20 h-full w-full cursor-pointer opacity-0" />-->
-<!--                        <div class="border-gray-200 peer-checked:border-primary-500 dark:border-gray-600 flex h-14 w-14 items-center justify-center rounded-full border-2 shadow-lg transition-all duration-300">-->
-<!--                            <img class="h-10 w-10 rounded-full" :src="`/assets/images/lang/spain.svg`" alt="flag icon" />-->
-<!--                        </div>-->
-<!--                        <div class="bg-primary-500 dark:border-gray-800 absolute -end-1 -top-1 hidden h-7 w-7 items-center justify-center rounded-full border-4 border-white text-white peer-checked:flex">-->
-<!--                            <svg data-v-26e5b7b0="" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="icon h-3 w-3" width="1em" height="1em" viewBox="0 0 24 24">-->
-<!--                                <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 6L9 17l-5-5"></path>-->
-<!--                            </svg>-->
-<!--                        </div>-->
-<!--                    </div>-->
-<!--                </div>-->
+                <div class="relative my-4 flex items-center justify-center">
+  <div class="relative">
+    <input 
+      type="radio" 
+      v-model="language" 
+      value="ru"
+      name="language_selection"
+      class="peer absolute start-0 top-0 z-20 h-full w-full cursor-pointer opacity-0"
+    />
+    <div class="border-gray-200 peer-checked:border-primary-500 dark:border-gray-600
+                flex h-14 w-14 items-center justify-center rounded-full border-2
+                shadow-lg transition-all duration-300">
+      <img class="h-10 w-10 rounded-full"
+           src="/assets/images/lang/russia.svg"
+           alt="Russian flag icon" />
+    </div>
+    <div class="bg-primary-500 dark:border-gray-800 absolute -end-1 -top-1 hidden h-7 w-7
+                items-center justify-center rounded-full border-4 border-white text-white
+                peer-checked:flex">
+      <!-- checkmark svg -->
+      <svg xmlns="http://www.w3.org/2000/svg"
+           aria-hidden="true"
+           role="img"
+           class="icon h-3 w-3"
+           width="1em"
+           height="1em"
+           viewBox="0 0 24 24">
+        <path fill="none" stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M20 6L9 17l-5-5" />
+      </svg>
+    </div>
+  </div>
+</div>
             </div>
             <div>
                 <img :src="`/assets/images/lang/translation.svg`" class="mx-auto w-full max-w-[280px] dark:hidden" alt="illustration" />
@@ -138,8 +161,8 @@
         },
         methods: {
             updateLangWithParam: async function(language) {
-                if(language === 'pt-br') {
-                    language = 'pt_BR';
+                if(language === 'ru') {
+                    language = 'ru';
                 }
                 loadLanguageAsync(language);
 

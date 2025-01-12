8b66f0733d4a14d267bd4101a2274ae98e5f2db4
Serhii Maslov <2021elit0049@ms.sumdu.edu.ua>
2025-01-12 22:53:15 +0200

    Upd4SettingsError

--- resources/views/filament/pages/settings.blade.php
+++ resources/views/filament/pages/settings.blade.php
@@ -1,13 +1,12 @@
-<x-filament-panels::page>
+@php
+    // This is a *different* resource altogether
+    // If you have a Setting model instance, pass it here; otherwise null is fine
+    $sidebar = \App\Filament\Resources\SettingResource::sidebar(null);
+@endphp
+
+<x-filament-page-with-sidebar::page :sidebar="$sidebar">
     <form wire:submit="submit">
         {{ $this->form }}
-
-        <br>
-        <x-filament-panels::form.actions
-            :actions="$this->getFormActions()"
-        />
-{{--        <x-filament::button type="submit" form="submit">--}}
-{{--            Обновить данные--}}
-{{--        </x-filament::button>--}}
+        <x-filament-panels::form.actions :actions="$this->getFormActions()" />
     </form>
-</x-filament-panels::page>
+</x-filament-page-with-sidebar::page>
